5c12902717a7cc2d775aff9cf837d3864eb90a4b
sm6303045-prog <sm6303045@gmail.com>
2026-05-31 16:03:47 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Card from "./card.js";
+
+export default function App() {
+  return (
+    <div>
+      <Card Name="pp1" Price="200" />
+      <Card Name="pp2" Price="1000" />
+    </div>
+  );
+}
